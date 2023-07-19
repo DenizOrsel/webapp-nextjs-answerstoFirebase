df869840125236e31dfc89c37c4848c69684af43
Deniz Orsel <denizonerorsel@gmail.com>
2023-07-19 10:57:40 +0200

    add active database listener

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,44 @@
 import React, { useState, useEffect } from "react";
+import firebase from "firebase/app";
+import "firebase/database";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  // Your Firebase config values (apiKey, authDomain, projectId, etc.)
+  projectId: "nfield-log",
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 function HomePage() {
   let [logData, setLogData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://nfield-log-default-rtdb.firebaseio.com/answerLog.json"
-      );
-      const data = await response.json();
-      const transformedData = [];
-      for (const key in data) {
-        transformedData.push({
-          id: key,
-          answer: data[key],
-        });
+    // Create a reference to your Firebase Realtime Database
+    const databaseRef = firebase.database().ref("answerLog");
+
+    // Define the real-time listener function
+    const handleDataChange = (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const transformedData = Object.entries(data).map(([id, answer]) => ({
+          id,
+          answer,
+        }));
+        setLogData(transformedData);
       }
-      setLogData(transformedData);
     };
-    fetchData();
+
+    // Attach the real-time listener
+    databaseRef.on("value", handleDataChange);
+
+    // Detach the listener when the component unmounts
+    return () => databaseRef.off("value", handleDataChange);
   }, []);
+
   return (
     <div>
       <h1>Nfield Logger</h1>
